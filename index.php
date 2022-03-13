546c4673d13669a18af2db027da8ee0357777d62
Admin <site@mail.com>
2022-03-13 21:44:58 +0300

    Add form elements to index.php

--- index.php
+++ index.php
@@ -3,17 +3,27 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="app/assets/css/app.min.css">
 	<title>Ajax Test</title>
 </head>
 <body>
+<form id="send" method="post" action="">
 
 	<div class="container">
-		<div class="header">Форма ввода данных</div>
-		<label for=""></label>
-		<input type="">
+		<div>Форма ввода данных</div>
+		<label for="name">Имя</label>
+		<input class="form-control" type="text" form="send" name="name">
+		<label for="email">Email</label>
+		<input class="form-control" type="text" form="send" name="email">
+		<label for="description">Описание</label>
+		<textarea class="form-control" form="send" name="description" rows="5"></textarea>
+		<input class="btn btn-primary" type="submit" form="send" value="Отправить">
+
 	</div>
 
+	<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
 	<script src="app/assets/js/app.min.js"></script>
 </body>
 </html>
