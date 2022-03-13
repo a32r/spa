025742a37463d63fbb38be18c676dae9c7975195
Admin <site@mail.com>
2022-03-13 17:12:42 +0300

    Start working on task

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="app/assets/css/app.min.css">
+	<title>Ajax Test</title>
+</head>
+<body>
+	
+	<script src="app/assets/js/app.min.js"></script>
+</body>
+</html>
