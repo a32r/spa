b1e6e53920a04b38990f26ccd7223124076c6a8a
Admin <site@mail.com>
2022-03-22 23:53:09 +0300

    Add div for nav and get blocks

--- index.php
+++ index.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="app/assets/css/app.min.css">
-	<title>Ajax Test</title>
-</head>
-<body>
-<form id="send" method="post" action="">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="app/assets/css/app.min.css">
+		<title>Ajax Test</title>
+	</head>
+	<body>
+		<form id="send" method="post" action="">
 
-	<div class="send">
-		<div>Форма ввода данных</div>
-		<div>
-			<label for="name">Имя</label>
-			<input class="form-control name" type="text" form="send" name="name" placeholder="Имя">
-		</div>
-		<div>
-			<label for="email">Email</label>
-			<input class="form-control email" type="text" form="send" name="email" placeholder="Email">
-		</div>
-		<div>
-			<label for="description">Описание</label>
-			<textarea class="form-control description" form="send" name="description" rows="5" placeholder="Описание"></textarea>
-		</div>
-		<input class="btn btn-primary" type="submit" form="send" value="Отправить">
-	</div>
+			<nav class="nav">
+				<li class="nav-item">Ввести данные</li>
+				<li class="nav-item">Вывести данные</li>
+			</nav>
 
-	<script src="app/assets/js/app.min.js"></script>
-</body>
+			<div class="send">
+				<div>Форма ввода данных</div>
+				<div>
+					<label for="name">Имя</label>
+					<input class="form-control name" type="text" form="send" name="name" placeholder="Имя">
+				</div>
+				<div>
+					<label for="email">Email</label>
+					<input class="form-control email" type="text" form="send" name="email" placeholder="Email">
+				</div>
+				<div>
+					<label for="description">Описание</label>
+					<textarea class="form-control description" form="send" name="description" rows="5" placeholder="Описание"></textarea>
+				</div>
+				<input class="btn btn-primary" type="submit" form="send" value="Отправить">
+			</div>
+
+			<div class="get">
+				<p>
+				Форма входа
+				</p>
+				<p>
+				Вывод данных
+				</p>
+			</div>
+
+			<script src="app/assets/js/app.min.js"></script>
+	</body>
 </html>
