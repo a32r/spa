4b89aa7763921538d64c5930514d4f34520ea48c
Admin <site@mail.com>
2022-03-23 23:22:50 +0300

    Add buttons for collapse send and get blocks. Add 0-main.scss

--- index.php
+++ index.php
@@ -8,38 +8,48 @@
 	</head>
 	<body>
 		<form id="send" method="post" action="">
+			<div class="container">
 
-			<nav class="nav">
-				<li class="nav-item">Ввести данные</li>
-				<li class="nav-item">Вывести данные</li>
-			</nav>
+				<div class="nav">
+					<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target=".send">
+						Ввести данные
+					</button>
+					<button class="btn btn-warning" type="button" data-bs-toggle="collapse" data-bs-target=".get">
+						Вывести данные
+					</button>
+				</div>
 
-			<div class="send">
-				<div>Форма ввода данных</div>
-				<div>
-					<label for="name">Имя</label>
-					<input class="form-control name" type="text" form="send" name="name" placeholder="Имя">
+				<div class="content">
+
+					<div class="send card">
+						<div>Форма ввода данных</div>
+						<div>
+							<label for="name">Имя</label>
+							<input class="form-control name" type="text" form="send" name="name" placeholder="Имя">
+						</div>
+						<div>
+							<label for="email">Email</label>
+							<input class="form-control email" type="text" form="send" name="email" placeholder="Email">
+						</div>
+						<div>
+							<label for="description">Описание</label>
+							<textarea class="form-control description" form="send" name="description" rows="5" placeholder="Описание"></textarea>
+						</div>
+						<input class="btn btn-primary" type="submit" form="send" value="Отправить">
+					</div>
+
+					<div class="get">
+						<p>
+						Форма входа
+						</p>
+						<p>
+						Вывод данных
+						</p>
+					</div>
+
 				</div>
-				<div>
-					<label for="email">Email</label>
-					<input class="form-control email" type="text" form="send" name="email" placeholder="Email">
-				</div>
-				<div>
-					<label for="description">Описание</label>
-					<textarea class="form-control description" form="send" name="description" rows="5" placeholder="Описание"></textarea>
-				</div>
-				<input class="btn btn-primary" type="submit" form="send" value="Отправить">
+
 			</div>
-
-			<div class="get">
-				<p>
-				Форма входа
-				</p>
-				<p>
-				Вывод данных
-				</p>
-			</div>
-
 			<script src="app/assets/js/app.min.js"></script>
 	</body>
 </html>
