cfb19fc0c9025e705c2145744f186abb5a9c695b
Admin <site@mail.com>
2022-04-27 00:01:54 +0300

    Add extra rows to users table in database

--- ajax1.sql
+++ ajax1.sql
@@ -61,7 +61,7 @@ CREATE TABLE `send` (
   `email` varchar(100) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `send` (
 
 LOCK TABLES `send` WRITE;
 /*!40000 ALTER TABLE `send` DISABLE KEYS */;
-INSERT INTO `send` VALUES (1,'5','5','5'),(2,'1','1','1');
+INSERT INTO `send` VALUES (1,'5','5','5'),(2,'1','1','1'),(3,'p','o','p');
 /*!40000 ALTER TABLE `send` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +87,7 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +96,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Первый','first@mail.com','Первый пользователь'),(2,'Второй','second@mail.com','Второй пользователь'),(3,'Третий','third@mail.com','Третий пользователь'),(4,'Четвертый','fourth@mail.com','Четвертый пользователь'),(5,'Пятый','fiveth@mail.com','Пятый пользователь'),(6,'Шестой','sixth@mail.com','Шестой пользователь'),(7,'Седьмой','seventh@mail.com','Седьмой пользователь'),(8,'Восьмой','eighth@mail.com','Восьмой пользователь');
+INSERT INTO `users` VALUES (1,'Первый','first@mail.com','Первый пользователь'),(2,'Второй','second@mail.com','Второй пользователь'),(3,'Третий','third@mail.com','Третий пользователь'),(4,'Четвертый','fourth@mail.com','Четвертый пользователь'),(5,'Пятый','fiveth@mail.com','Пятый пользователь'),(6,'Шестой','sixth@mail.com','Шестой пользователь'),(7,'Седьмой','seventh@mail.com','Седьмой пользователь'),(8,'Восьмой','eighth@mail.com','Восьмой пользователь'),(9,'Девятый','nineth@mail.com','Девятый пользователь'),(10,'Десятый','tenth@mail.com','Десятый пользователь'),(11,'Одиннадцатый','eleventh@mail.com','Одиннадцатый пользователь'),(12,'Двенадцатый','twelfth@mail.com','Двенадцатый пользователь'),(13,'Тринадцатый','thirteenth@mail.com','Тринадцатый пользователь'),(14,'Четырнадцатый','fourteenth@mail.com','Четырнадцатый пользователь'),(15,'Пятнадцатый','fifteenth@mail.com','Пятнадцатый пользователь'),(16,'Шестнадцатый','sixteenth@mail.com','Шестнадцатый пользователь'),(17,'Семнадцатый','seventeenth@mail.com','Семнадцатый пользователь'),(18,'Восемнадцатый','eighteenth@mail.com','Восемнадцатый пользователь'),(19,'Девятнадцатый','nineteenth@mail.com','Девятнадцатый пользователь'),(20,'Двадцатый','twentieth@mail.com','Двадцатый пользователь'),(21,'Двадцать первый','twentyfirst@mail.com','Двадцать первый пользователь'),(22,'Двадцать второй','twentysecond@mail.com','Двадцать второй пользователь'),(23,'Двадцать третий','twentythird@mail.com','Двадцать третий пользователь');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -109,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-11  0:54:15
+-- Dump completed on 2022-04-27 22:51:42
--- app/php/send.php
+++ app/php/send.php
@@ -3,11 +3,13 @@
 require_once('functions.php');
 require_once('db_connect.php');
 
+$table = 'send';
+
 if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['description'])) {
 	$name = htmlspecialchars($_POST['name']);
 	$email = $_POST['email'];
 	$description = htmlspecialchars($_POST['description']);
-	$query = "INSERT INTO send VALUES (NULL, ?, ?, ?)";
+	$query = "INSERT INTO $table VALUES (NULL, ?, ?, ?)";
 	$stmt = $db->prepare($query);
 	$stmt->execute([$name, $email, $description]);
 
